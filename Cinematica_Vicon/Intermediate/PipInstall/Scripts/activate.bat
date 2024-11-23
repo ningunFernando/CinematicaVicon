92ef7d43766b6618358755f2eeac996cf765fa05
Cristian Uriel Salazar Chavez <a21310215@ceti.mx>
2024-11-23 16:54:42 -0600

    Prueba
    xdxd

--- Cinematica_Vicon/Intermediate/PipInstall/Scripts/activate.bat
+++ Cinematica_Vicon/Intermediate/PipInstall/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ferna\OneDrive\Escritorio\CinematicaVicon\Cinematica_Vicon\Intermediate\PipInstall
+set VIRTUAL_ENV=C:\Users\uetc2\Documents\GitHub\CinematicaVicon\Cinematica_Vicon\Intermediate\PipInstall
 
 if not defined PROMPT set PROMPT=$P$G
 
